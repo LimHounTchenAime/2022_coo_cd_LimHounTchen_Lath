20c971eeb6a758badedfeb4faf6ae6a7ebe453a1
victorLATH <victor.lath9@etu.univ-lorraine.fr>
2022-05-11 09:02:30 +0200

    classe test

---
+++ src/test/testChargerMagasin.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testChargerMagasin {
+
+    @Test
+    public void chargerMagasin(){
+        assertEquals();
+
+
+    }
+
+}
